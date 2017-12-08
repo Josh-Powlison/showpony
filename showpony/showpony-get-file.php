f9c79969cc24daba36bf160caef3d50e2b1ec7c9
Josh Powlison <joshuapowlison@gmail.com>
2017-12-08 18:34:49 -0500

    Adjust the layout of files and ignore test resources

--- showpony/showpony-get-file.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$file=dirname(__FILE__,2).'/'.$_GET['get'];
-
-	#These headers are required to scrub media (yes, you read that right)
-	header('Accept-Ranges: bytes');
-	header('Content-Length:'.filesize($file));
-	
-	readfile($file);
-?>
