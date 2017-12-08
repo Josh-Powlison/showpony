d6920f00272de8d70e7057ee048930bee2966f0d
Josh Powlison <joshuapowlison@gmail.com>
2017-12-08 17:12:57 -0500

    Condense, begin adding RSS feed support

--- showpony/showpony-classes.php
+++ showpony/showpony-classes.php
@@ -8,15 +8,12 @@ class Showpony{
 	
 	#Initialize the class
 	function __construct($inputArray){
-		#The message will be kept in the output buffer. We will send the output buffer to the user in the "message".
-		ob_start();
-		
 		$this->admin=false;
-		chdir(dirname(__FILE__,2).'/'.($inputArray['path'] ?? $_POST['path'] ?? null));
+		chdir(dirname(__FILE__,2).'/'.($inputArray['path'] ?? $_POST['path'] ?? $_GET['path'] ?? null));
 		
 		#This object is sent to the user as JSON
 		$response=[
-			'call' => ($_POST['showpony-call'] ?? null)
+			'call' => ($_POST['showpony-call'] ?? $inputArray['call'] ?? null)
 			,'success' => false
 			,'message' => null
 		];
@@ -26,6 +23,9 @@ class Showpony{
 		
 		#If the user passed a call
 		if(!empty($_POST['showpony-call'])){
+			#The message will be kept in the output buffer. We will send the output buffer to the user in the "message".
+			ob_start();
+			
 			#If we were here to login, don't bother doing so again
 			if($response['call']!=='login'){
 				$response=$this->{$response['call']}($response);
@@ -35,6 +35,8 @@ class Showpony{
 			
 			echo json_encode($response);
 		}
+		
+		if(!empty($_GET['rss'])) $this->RSS();
 	}
 	
 	protected function login($response){
@@ -72,7 +74,7 @@ class Showpony{
 			){
 				#Get the file names and pass them on
 				$response['success']=true;
-				$response['files']=$this->getFiles();
+				$response=$this->getFiles($response);
 			}
 			
 			$response['admin']=$this->admin;
@@ -83,7 +85,7 @@ class Showpony{
 	
 	protected function logout($response){
 		$response["success"]=setcookie('showpony_password',null,-1);
-		$response["files"]=$this->getFiles();
+		$response=$this->getFiles($response);
 		return $response;
 	}
 	
@@ -131,8 +133,9 @@ class Showpony{
 	}
 	
 	#Get files and protect others
-	public function getFiles(){
-		$files=[];
+	public function getFiles($response){
+		$response['files']=[];
+		$response['success']=true;
 		
 		#Run through the files (backwards, so we can splice the array and keep going back)
 		foreach(scandir('.') as &$file){
@@ -147,17 +150,17 @@ class Showpony{
 				#Should be live; remove any x at the beginning of the filename
 				if(strtotime($date[0].' UTC')<=time()){
 					if($file[0]=='x'){
-						rename(
-							$file
-							,substr($file,1)
+						$response['success']=(
+							$response['success']
+							&& rename($file,substr($file,1))
 						);
 						$file=substr($file,1);
 					}
 				}else{ #Shouldn't be live; make sure an x is at the beginning of the filename
 					if($file[0]!="x"){
-						rename(
-							$file
-							,'x'.$file
+						$response['success']=(
+							$response['success']
+							&& rename($file,'x'.$file)
 						);
 						$file='x'.$file;
 					}
@@ -168,14 +171,119 @@ class Showpony{
 			}
 			
 			#Add the file to the array
-			$files[]=$file;
+			$response['files'][]=$file;
 		}
 		
-		return $files;
+		return $response;
+	}
+	
+	#http://localhost/showpony/showpony/showpony-classes.php?rss=true&path=parts/
+	public function RSS(){
+		die("RSS feed incomplete.");
+		
+		#Parse the results as XML
+		#header("Content-type: application/xml");
+		header("Content-type: txt");
+		
+		$cover="http://joshpowlison.com/podcasts/pickapart-tv/cover.jpg";
+		$creator="Josh Powlison";
+		$email="joshuapowlison@gmail.com";
+		$category1="TV &amp; Film";
+		$category2="Education";
+		$explicit="no";
+		$title="Inspector Josh Investigates TV";
+		$language="en-us";
+		
+		$keywords="videogames,announcement,games,media";
+		
+		$summary="Inspector Josh studies TV shows for writing lessons.";
+		$subtitle="Inspector Josh will find the truth.";
+		
+		?><rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
+			<channel>
+				<atom:link href="<?=getcwd()?>" rel="self" type="application/rss+xml"/>
+				<title><?=$title?></title>
+				<link><?=getcwd()?></link>
+				<description>Inspector Josh studies TV shows for writing lessons.</description>
+				<language><?=$language?></language>
+				<webMaster><?=$email?> (<?=$creator?>)</webMaster>
+				<copyright><![CDATA[ Copyright 2017 Josh Powlison. All rights reserved. ]]></copyright>
+				<docs><?=getcwd()?></docs>
+				<managingEditor><?=$email?> (<?=$email?>)</managingEditor>
+				<image>
+					<url><?=$cover?></url>
+					<title><?=$title?></title>
+					<link><![CDATA[ <?=getcwd()?> ]]></link>
+				</image>
+				<itunes:summary><![CDATA[ <?=$summary?> ]]></itunes:summary>
+				<itunes:author><?=$creator?></itunes:author>
+				<itunes:category text="<?=$category1?>"/>
+				<itunes:category text="<?=$category2?>"/>
+				<itunes:image href="<?=$cover?>"/>
+				<itunes:explicit><?=$explicit?></itunes:explicit>
+				<itunes:owner>
+					<itunes:name><![CDATA[ <?=$creator?> ]]></itunes:name>
+					<itunes:email><?=$email?></itunes:email>
+				</itunes:owner>
+				<itunes:subtitle><![CDATA[ <?=$subtitle?> ]]></itunes:subtitle>
+
+				<?
+				
+				foreach($this->getFiles($response)['files'] as $file){
+					
+					$name;
+					$date;
+					$length;
+					
+					#Get the name and date from the filename
+					preg_match('/(?<=\().+(?=\))/',$file,$name);
+					preg_match('/[^x][^(]+(?!\()\S?/',$file,$date);
+					preg_match('/[^\s)]+(?=\..+$)/',$file,$length);
+					
+					#Ignore hidden files
+					if($file[0]==='x') continue;
+					
+					$name=$name[0] ?? '';
+					$date=$date[0] ? strtotime($date[0]) : filemtime($file);
+					str_replace(';',':',$date);
+					$length=$length[0] ?? 10;
+					
+					#die(date('D, j M Y G:i:s',strtotime($date[0])));
+					
+					$date=date('D, j M Y G:i:s',$date);
+					?>
+				<item>
+					<title><?=$name?></title>
+					<link><?=getcwd().'/'.$file?></link>
+					<guid><?=getcwd().'/'.$file?></guid>
+					<pubDate><?=$date?></pubDate>
+					<description><![CDATA[ Listen to the latest entry of <?=$title?>! ]]></description>
+					<enclosure length="<?=$length[0] ?>" type="audio/mpeg" url="<?=getcwd().'/'.$file?>"/>
+					<itunes:image href="<?=$cover?>"/>
+					<itunes:duration><?=($length / 60).':'.$length[0] % 60 ?></itunes:duration>
+					<itunes:explicit><?=$explicit?></itunes:explicit>
+					<itunes:keywords><?=$keywords?></itunes:keywords>
+					<itunes:subtitle><![CDATA[ The latest update is here! ]]></itunes:subtitle>
+				</item>
+					<?
+				}
+				?>
+			</channel>
+		</rss>
+		<?
 	}
 }
 
 #Call this file to run Showpony functions
-if(!empty($_POST['showpony-call'])) new Showpony([]);
+if(!empty($_POST['showpony-call']) || !empty($_GET['rss'])) new Showpony([]);
 
+if(!empty($_GET['showpony-get'])){
+	$file=dirname(__FILE__,2).'/'.$_GET['showpony-get'];
+
+	#These headers are required to scrub media (yes, you read that right)
+	header('Accept-Ranges: bytes');
+	header('Content-Length:'.filesize($file));
+	
+	readfile($file);
+}
 ?>
