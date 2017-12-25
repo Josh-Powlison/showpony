00918f14aeade1744f0b5d3116197e6ec8c5a147
Josh Powlison <joshuapowlison@gmail.com>
2017-12-24 23:44:45 -0500

    Remove hashes from PHP code, condense
    The PHP file is made specifically for being called through AJAX, and isn't very useful for programmers to link to in their own PHP files (the getFiles function could be the exception). I've removed the class here and made it a simpler file, to try to make things more straightforward and add a touch of performance.
    
    Hashes were removed because in this context, it really didn't make sense. I'd need to create a whole account system for users at that point to allow hashes, and possible MySQL requirements, etc; I couldn't think of a good way to use hashes that overall INCREASED security without likely adding more holes. Plain-text password in the PHP file isn't great, but I think in this case trying to work with that would just add even MORE security vulnerabilities. If users just use a unique password, then if they're hacked they'll be fine.

--- showpony/showpony.php
+++ showpony/showpony.php
@@ -4,119 +4,96 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
-class Showpony{
-	protected static $hash='$2y$10$CiuxajafBgiE5Y7pha9p5.AD185.Bxnk6GLyhPehGH1k0sa2LZ6fm'; #NULL will disable admin access. Using a string will set that as the password and allow admin access.
+#NULL will disable admin access. Using a string will set that as the password and allow admin access.
+static $password='password';
+
+#Log out if there's no password
+if(!$password) unset($_SESSION['showpony_admin']);
+
+#Get a private file (if admin)
+if(!empty($_GET['get'])){
+	if(empty($_SESSION['showpony_admin'])) die("You need to be logged in to access private files.");
 	
-	#Initialize the class
-	function __construct($inputArray){
-		#Log out if there's no password
-		if(!self::$hash) unset($_SESSION['showpony_admin']);
-		
-		#This object is sent to the user as JSON
-		$response=[
-			'call' => ($inputArray['call'] ?? null)
-			,'success' => false
-			,'directory' => dirname(__FILE__,2).'/'.($inputArray['path'] ?? '')
-		];
-		
-		chdir($response['directory']);
-		
-		#If the user passed a call
-		if($response['call']){
-			#We'll store all errors and code that's echoed, so we can send that info to the user (in a way that won't break the JSON object).
-			ob_start();
-			
-			#Safe functions
-			if($response['call']==='logout' || $response['call']==='getFiles'){
-				if($response['call']==='logout') unset($_SESSION['showpony_admin']);
-				$response=$this->getFiles($response);
-			}else{ #Admin functions
-				#If no password, exit
-				if(!self::$hash || ($response['call']!=='login' && empty($_SESSION['showpony_admin']))) echo "You aren't logged in or don't have admin set up! Try refreshing and logging in again, or check out Showpony's wiki on GitHub for setting up or disabling admin.";
-				else{
-					#Try logging in with the passed password
-					if($response['call']==='login'){
-						#If the password's right, set the session and get the files
-						if($_SESSION['showpony_admin']=password_verify($_POST['password'],self::$hash)) $response=$this->getFiles($response);
-						#Wrong password
-						else echo 'Wrong password!';
-					}
-					
-					#Try renaming the file, and pass the new filename to the user
-					elseif($response['call']==='renameFile') $response['success']=rename($_POST['name'],$response['file']=$_POST['newName']);
-
-					#Delete the old file, upload the new file with the old name and new extension
-					elseif($response['call']==='uploadFile') $response["success"]=unlink($_POST['name']) && move_uploaded_file($_FILES["files"]["tmp_name"],$response["file"]=pathinfo($_POST['name'],PATHINFO_FILENAME).'.'.pathinfo($_FILES['files']['name'],PATHINFO_EXTENSION));
-					
-					#Create a new file for the user to edit and update. We store that new file name in a variable.
-					elseif($response['call']==='newFile') $response["success"]=file_put_contents($response["file"]='x2038-01-01 20;00;00 (Untitled '.time().').html','Replace me with your new, better file!');
-					
-					#Delete a file
-					elseif($response['call']==='deleteFile') $response["success"]=unlink($_POST['name']);
-				}
-			}
-			
-			$response["message"]=ob_get_clean();
-			$response['admin']=$_SESSION['showpony_admin'] ?? false;
-			
-			echo json_encode($response);
-		}
-	}
-	
-	#Get files and protect others
-	public function getFiles($response){
-		$response['files']=[];
-		$response['success']=true;
-		
-		#Run through the files (backwards, so we can splice the array and keep going back)
-		foreach(scandir('.') as &$file){
-			#Ignore folders and hidden files
-			if($file[0]==".") continue;
-
-			#Ignore files that have dates in their filenames set to later
-			$date;
-			if(preg_match('/[^x][^(]+(?!\()\S?/',$file,$date)){ #Get the posting date from the file's name; if there is one:
-				$date=str_replace(';',':',$date); #Semicolons are used instead of colons to support Windows file naming
-				
-				#Should be live; remove any x at the beginning of the filename
-				if(strtotime($date[0].' UTC')<=time()){
-					if($file[0]=='x' && !rename($file,$file=substr($file,1))) $response['success']=false;
-				}else{
-				#Shouldn't be live; make sure an x is at the beginning of the filename
-					if($file[0]!="x" && !rename($file,$file='x'.$file)) $response['success']=false;
-					
-					#Don't add hidden files if we aren't logged in
-					if(!$_SESSION['showpony_admin']) continue;
-				}
-			}
-			
-			#Add the file to the array
-			$response['files'][]=$file;
-		}
-		
-		return $response;
-	}
-}
-
-#Call this file to run Showpony functions
-if(!empty($_POST['showpony-call'])){
-	new Showpony([
-		'call'=>$_POST['showpony-call']
-		,'path'=>$_POST['path']
-	]);
-}
-
-if(!empty($_GET['showpony-get'])){
-	if(empty($_SESSION['showpony_admin'])){
-		die("You need to be logged in to access private files.");
-	}
-	
-	$file=dirname(__FILE__,2).'/'.$_GET['showpony-get'];
+	$file=dirname(__FILE__,2).'/'.$_GET['get'];
 
 	#These headers are required to scrub media (yes, you read that right)
 	header('Accept-Ranges: bytes');
 	header('Content-Length:'.filesize($file));
 	
 	readfile($file);
+}else{
+	#Run called functions
+	
+	#This object is sent to the user as JSON
+	$response=[];
+	
+	#Go to the files directory
+	chdir(dirname(__FILE__,2).'/'.($_POST['path'] ?? $_GET['get']));
+	
+	#We'll store all errors and code that's echoed, so we can send that info to the user (in a way that won't break the JSON object).
+	ob_start();
+	
+	if($_POST['call']==='logout') unset($_SESSION['showpony_admin']);
+	
+	if(
+		$_POST['call']==='login'
+		|| $_POST['call']==='logout'
+		|| $_POST['call']==='getFiles'
+	){
+		#On login request, attempt to log in; if it fails, let the user know
+		if($_POST['call']==='login' && empty($_SESSION['showpony_admin']=$_POST['password']===$password)) echo 'Wrong password!';
+		else{
+			#Get files and protect others
+			$response['files']=[];
+			$response['success']=true;
+			
+			#Run through the files
+			foreach(scandir('.') as &$file){
+				#Ignore folders and hidden files
+				if($file[0]==".") continue;
+
+				#Ignore files that have dates in their filenames set to later
+				$date;
+				if(preg_match('/[^x][^(]+(?!\()\S?/',$file,$date)){ #Get the posting date from the file's name; if there is one:
+					#If the time is previous to now (replacing ; with : for Windows filename compatibility)
+					if(strtotime(str_replace(';',':',$date)[0].' UTC')<=time()){
+						#Should be live; remove any x at the beginning of the filename
+						if($file[0]=='x' && !rename($file,$file=substr($file,1))) $response['success']=false;
+					}else{
+						#Shouldn't be live; make sure an x is at the beginning of the filename
+						if($file[0]!="x" && !rename($file,$file='x'.$file)) $response['success']=false;
+						
+						#Don't add hidden files if we aren't logged in
+						if(!$_SESSION['showpony_admin']) continue;
+					}
+				}
+				
+				#Add the file to the array
+				$response['files'][]=$file;
+			}
+		}
+	}
+	#Admin functions: must be logged in
+	else{
+		if(empty($_SESSION['showpony_admin'])) echo "You aren't logged in or don't have admin set up! Try refreshing and logging in again, or check out Showpony's wiki on GitHub for setting up or disabling admin.";
+		
+		#Try renaming the file, and pass the new filename to the user
+		elseif($_POST['call']==='renameFile') $response['success']=rename($_POST['name'],$response['file']=$_POST['newName']);
+
+		#Delete the old file, upload the new file with the old name and new extension
+		elseif($_POST['call']==='uploadFile') $response["success"]=unlink($_POST['name']) && move_uploaded_file($_FILES["files"]["tmp_name"],$response["file"]=pathinfo($_POST['name'],PATHINFO_FILENAME).'.'.pathinfo($_FILES['files']['name'],PATHINFO_EXTENSION));
+		
+		#Create a new file for the user to edit and update. We store that new file name in a variable.
+		elseif($_POST['call']==='newFile') $response["success"]=file_put_contents($response["file"]='x2038-01-01 20;00;00 (Untitled '.time().').html','Replace me with your new, better file!');
+		
+		#Delete a file
+		elseif($_POST['call']==='deleteFile') $response["success"]=unlink($_POST['name']);
+	}
+	
+	$response['message']=ob_get_clean();
+	$response['admin']=$_SESSION['showpony_admin'] ?? false;
+	
+	echo json_encode($response);
 }
+
 ?>
