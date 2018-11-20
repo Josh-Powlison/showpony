ec79bd1e1e86548899556ac8f42a914d2b70195b
Josh Powlison <joshuapowlison@gmail.com>
2018-11-20 13:55:44 -0500

    Change P to M (Player to Module) in template module

--- showpony/modules/unused/template.php
+++ showpony/modules/unused/template.php
@@ -19,61 +19,61 @@ function templateUnhideChildren($input){
 ?>
 
 S.<?php echo 'template'; ?>=new function(){
-	const P=this; //The media Player
+	const M=this; //The media Player
 	
-	P.currentTime=null;
-	P.currentFile=null;
+	M.currentTime=null;
+	M.currentFile=null;
 	
-	P.window=document.createElement('div'); //Or other element
-	P.window.className='showpony-block'; //Or other class
+	M.window=document.createElement('div'); //Or other element
+	M.window.className='showpony-block'; //Or other class
 	
-	P.play=function(){
+	M.play=function(){
 		//How the medium plays. Can remain empty
 	}
 	
-	P.pause=function(){
+	M.pause=function(){
 		//How the medium pauses. Can remain empty
 	}
 	
 	//What happens when the user activates story input (clicking, spacebar, etc)
-	P.input=function(){
+	M.input=function(){
 		S.toggle(); //Toggles the menu
 	}
 	
 	//Update the time in the current file. May happen automatically, on scrolling, etc
-	P.timeUpdate=function(time=0){
-		P.currentTime=P.window.currentTime=time;
+	M.timeUpdate=function(time=0){
+		M.currentTime=M.window.currentTime=time;
 	}
 	
-	P.src=function(file=0,time=0){
+	M.src=function(file=0,time=0){
 		return new Promise(function(resolve,reject){
 			//Change the file if it'd be a new one
-			if(P.currentFile!==file){
+			if(M.currentFile!==file){
 				var filePath=S.files[file].path;
 			}
 			
 			//If we're not paused, play
-			if(!S.paused) P.play();
+			if(!S.paused) M.play();
 			
 			resolve();
 		});
 	}
 	
-	P.displaySubtitles=function(){
+	M.displaySubtitles=function(){
 		if(S.currentSubtitles===null){
 			subtitles.innerHTML='';
 			return;
 		}
 		
-		if(S.files[P.currentFile].subtitles){
+		if(S.files[M.currentFile].subtitles){
 			//Add them in
 		}else{
 			//If don't have the file
-			fetch(S.subtitles[S.currentSubtitles]+S.files[P.currentFile].title+'.vtt')
+			fetch(S.subtitles[S.currentSubtitles]+S.files[M.currentFile].title+'.vtt')
 			.then(response=>{return response.text();})
 			.then(text=>{
-				S.files[P.currentFile].subtitles=text;
-				P.displaySubtitles();
+				S.files[M.currentFile].subtitles=text;
+				M.displaySubtitles();
 			});
 		}
 	}
