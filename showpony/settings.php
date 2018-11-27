7c113a3254303f4e597ba51bdbb5eaed4bafd4f8
Josh Powlison <joshuapowlison@gmail.com>
2018-11-27 13:26:03 -0500

    Use ~ to hide files by default instead of x

--- showpony/settings.php
+++ showpony/settings.php
@@ -8,7 +8,7 @@ ini_set('display_errors',1);
 const ROOT=__DIR__;
 const DEFAULT_STORIES_PATH='files/';
 const DEFAULT_LANGUAGE='en';
-const HIDDEN_FILENAME_STARTING_CHAR='x'; // Change in .htaccess too, to block direct URL access
+const HIDDEN_FILENAME_STARTING_CHAR='~'; // Change in .htaccess too, to block direct URL access
 
 const FILE_DATA_GET_MODULE=[
 	'default'			=>	null
