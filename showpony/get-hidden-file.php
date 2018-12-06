4dd084816933ebb4337fce4d4a35a6b7bc49d333
Josh Powlison <joshuapowlison@gmail.com>
2018-12-06 02:31:36 -0500

    Rename and tweak

--- showpony/get-hidden-file.php
+++ showpony/get-hidden-file.php
@@ -6,10 +6,10 @@ session_start();
 if(empty($_SESSION['showpony_admin'])) die('You need to be logged in to access private files.');
 
 // Go to the correct directory
-chdir(($_GET['rel-path'] ?: '..').'/');
+chdir(($_GET['rel-path'] ?? '..').'/');
 
 // Get the file path
-$file=dirname(__FILE__,2).'/'.$_GET['get'];
+$file=dirname(__FILE__,2).'/'.$_GET['file'];
 
 // These headers are required to scrub media (yes, you read that right)
 header('Accept-Ranges: bytes');
