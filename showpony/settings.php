a439fa65719c92510a357f04cddf42189bd68094
Josh Powlison <joshuapowlison@gmail.com>
2018-12-10 16:41:34 -0500

    Recomment for clarity, and use "story" as default folder name instead of "files" (it's more clear)

--- showpony/settings.php
+++ showpony/settings.php
@@ -5,12 +5,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
-date_default_timezone_set('UTC');
+date_default_timezone_set('UTC');			// The timezone we're reading times in
 
-const ROOT=__DIR__;
-const DEFAULT_PATH='files/';
-const DEFAULT_LANGUAGE='en';
-const DEFAULT_FILE_DURATION=10;				// How long files without length data will be assumed to be
+const ROOT=__DIR__;							// The directory Showpony is in
+const DEFAULT_PATH='story/';				// The path to the files we'll play
+const DEFAULT_LANGUAGE='en';				// If a language isn't set by the user, assume this one
+const DEFAULT_FILE_DURATION=10;				// How long files without a duration in their filename will be assumed to be
 const PRELOAD_BYTES=.5*1048576;				// How many file bytes to preload
 const RELEASE_DATES=1;						// How many upcoming files to show release dates for
 const HIDDEN_FILENAME_STARTING_CHAR='~';	// Change in .htaccess too, to block direct URL access
