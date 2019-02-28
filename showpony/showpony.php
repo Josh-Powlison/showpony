fdacf8a983b186b7139c3435dffa03ed931c4236
Josh Powlison <joshuapowlison@gmail.com>
2019-02-28 17:29:51 -0500

    Have $_GET path replace the default path rather than appending it

--- showpony/showpony.php
+++ showpony/showpony.php
@@ -2,7 +2,7 @@
 
 require 'settings.php';
 
-define('STORIES_PATH'	, DEFAULT_PATH.($_GET['path'] ?? ''));
+define('STORIES_PATH'	, $_GET['path'] ?? DEFAULT_PATH);
 
 // Get the query from the paths
 define('NAME'			, preg_match('/[^\/]+(?=\/?$)/',STORIES_PATH,$match) ? $match[0] : 'story');
