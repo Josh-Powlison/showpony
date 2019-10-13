358bb812e6c02662a279069857beca9561cbcfda
Josh Powlison <joshuapowlison@gmail.com>
2019-10-13 17:36:53 -0400

    Unhide VN files properly (syntax was updated long ago, this file had to be too)

--- showpony/modules/visualNovel/functions.php
+++ showpony/modules/visualNovel/functions.php
@@ -3,10 +3,17 @@
 function visualNovelUnhideChildren($input){
 	// visualNovel includes files on specific lines
 	
-	$handle=fopen($input,'r');
-	$comment=false;
+	$handle = fopen($input,'r');
+	$comment = false;
+	
+	// A list of all the objects
+	$objects = [
+		'textbox'	=> 'textbox',
+		'engine'	=> 'engine'
+	];
 
-	while(($line=fgets($handle))!==false){
+	// Go through every line
+	while(($line = fgets($handle)) !== false){
 		// Remove line breaks on line
 		$line=trim($line,"\r\n");
 		
@@ -30,25 +37,68 @@ function visualNovelUnhideChildren($input){
 			continue;
 		}
 		
-		// Determines the correct file type, and allows ids for multiple objects (mook#1, mook#2)
-		
-		// Characters images
-		if(preg_match('/^([^\s\.\=#]+)(#\S*)?\t+(.+)$/',$line,$matches)){
-			// Split layers so we can grab every file
-			$layers=explode(',',$matches[3]);
-			foreach($layers as $layer){
-				unhideFile('resources/characters/'.$matches[1].'/'.$layer.'.png');
+		// Get object data on the line
+		if(preg_match('/([^\t\.]*)?\.?([^\t]*)?\t+(.+)/',$line,$matches)){
+			$type = null;
+			
+			// Check if it's a textbox
+			if($matches[1] == ''){
+				$type = 'textbox';
 			}
-		}
-		
-		// Backgrounds
-		else if(preg_match('/^([^\.\s#]+)(#[^.\s]*)?$/',$line,$matches)){
-			unhideFile('resources/backgrounds/'.$matches[1].'.jpg');
-		}
-		
-		// Audio
-		else if(preg_match('/^([^\s#]+)(#\S*)?\.(loop|play)$/',$line,$matches)){
-			unhideFile('resources/audio/'.$matches[1].'.mp3');
+			// Check if it's type is engine
+			else if($matches[1] == 'engine'){
+				$type = 'engine';
+				
+				// See what type of object is being created and add it to the list
+				switch($matches[2]){
+					case 'textbox':
+					case 'character':
+					case 'audio':
+						$objects[$matches[3]] = $matches[2];
+						break;
+					default:
+						break;
+				}
+			}
+			// See if the object already exists
+			else if(array_key_exists($matches[1],$objects)){
+				$type = $objects[$matches[1]];
+			}
+			// Figure out the type if it's a new object
+			else{
+				// If it includes a variable, skip over it
+				if(preg_match('/\[/',$matches[1]) == true) $type = 'variableGet';
+				// If it ends with =, it's a variable
+				else if(preg_match('/=/',$matches[1]) == true) $type = 'variableSet';
+				// If it mentions an audio type, it's audio
+				else if(
+					preg_match('/\.(?:mp3|ogg|wav)/i',$matches[3]) == true
+				) $type = 'audio';
+				// All other types are characters
+				else $type = 'character';
+				
+				$objects[$matches[1]] = $type;
+			}
+			
+			// Get audio and images, if the command is content
+			if(
+				($matches[2] == 'content' || $matches[2] == '')
+				&& ($type == 'character' || $type == 'audio')
+			){
+				$layers = explode(',',$matches[3]);
+				
+				foreach($layers as $layer){
+					$path = 'resources';
+					if($layer[0] == '/'){
+						$path .= $layer;
+					} else $path .= '/' . ($type == 'character' ? 'images/' . $matches[1] . '/' . $layer : 'audio/' . $layer);
+					
+					// Assume extension based on character or audio, if one isn't set
+					if(preg_match('/\./',$layer) == false) $path .= ($type == 'character' ? '.png' : '.mp3');
+					
+					unhideFile($path);
+				}
+			}
 		}
 		
 		// Other
